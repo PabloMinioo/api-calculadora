d36b0a6105b07a1d6d4179d9b79df84ff553e3c2
PabloMinioo <pabloagustinm@hotmail.com>
2026-07-16 12:03:54 -0300

    Agregamos la clase principal

---
+++ src/main/java/api_calculadora/ApiCalculadoraApplication.java
@@ -0,0 +1,13 @@
+package api_calculadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCalculadoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCalculadoraApplication.class, args);
+	}
+
+}
